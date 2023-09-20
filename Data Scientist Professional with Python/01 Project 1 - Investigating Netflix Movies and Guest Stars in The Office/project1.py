faa644f2d6840b413ded8d799f0627a2c5b9a535
ekmanch <christian.ekman89@gmail.com>
2023-09-20 22:28:19 +0200

    added labels and title etc to plot

--- Data Scientist Professional with Python/01 Project 1 - Investigating Netflix Movies and Guest Stars in The Office/project1.py
+++ Data Scientist Professional with Python/01 Project 1 - Investigating Netflix Movies and Guest Stars in The Office/project1.py
@@ -9,6 +9,7 @@ office_episodes = pd.read_csv('datasets/office_episodes.csv')
 episode_number = np.array(office_episodes['episode_number'])
 viewerships = np.array(office_episodes['viewership_mil'])
 scaled_ratings = np.array(office_episodes['scaled_ratings'])
+has_guests = np.array(office_episodes['has_guests'])
 
 # Create an array with correct coloring based on the scaled rating for each episode
 col =  np.empty([len(episode_number)], dtype=object)
@@ -17,6 +18,17 @@ col[np.logical_and(scaled_ratings >=0.25, scaled_ratings < 0.5)] = "orange"
 col[np.logical_and(scaled_ratings >=0.5, scaled_ratings < 0.75)] = "lightgreen"
 col[scaled_ratings >= 0.75] = "darkgreen"
 
+# Create an array with correct bubble size based on guest appearances for each episode
+size =  np.zeros([len(episode_number)])
+size[has_guests == True] = 250
+size[has_guests == False] = 25
+
+# Have to calculate this index nicer...
+print(np.where(viewerships == viewerships.max()))
+
 # Configure scatterplot and show
-plt.scatter(episode_number, viewerships, c=col)
+plt.scatter(episode_number, viewerships, s=size, c=col)
+plt.title("Popularity, Quality, and Guest Appearances on the Office")
+plt.xlabel("Episode Number")
+plt.ylabel("Viewership (Millions)")
 plt.show()
